a52607a6f2fbf884e600ea9f6b630f5281ac6b91
Samueru-sama <xdglawyer@outlook.com>
2026-08-22 19:56:54 -0400

    build manually

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,16 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	autoconf-archive \
+	cmake            \
+	nasm             \
+	ninja            \
+	python           \
+	qt6-positioning  \
+	rust             \
+	tar              \
+	zip
 
 if [ "$ARCH" = 'x86_64' ]; then
 	pacman -Syu --noconfirm libva-intel-driver
@@ -16,16 +25,46 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini ffmpeg-mini
 
-# Comment this out if you need an AUR package
-pacman -Rdd --noconfirm libpng || :
-make-aur-package --chaotic-aur ladybird
-
 # If the application needs to be manually built that has to be done down here
+echo "Building Ladybird..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/LadybirdBrowser/ladybird ./ladybird
+cd ./ladybird
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+VERSION=r$(git rev-list --count HEAD).$(git rev-parse --short HEAD)
+echo "$VERSION" > ~/version
+
+# vcpkg is used to build all the third party dependencies statically,
+# the checkout needs to match the builtin-baseline of the manifest
+git clone https://github.com/microsoft/vcpkg.git ./vcpkg
+git -C ./vcpkg checkout "$(awk -F'"' '/"builtin-baseline"/{print $4; exit}' vcpkg.json)"
+
+export VCPKG_ROOT="$PWD/vcpkg"
+export VCPKG_DISABLE_METRICS="true"
+export RUSTUP_TOOLCHAIN=stable
+
+# Apply required patches:
+# From the AUR 'ladybird' package:
+# - link-static-harfbuzz-fontconfig: static vcpkg build needs these symbols force-included
+# - gcc-wno-restrict: GCC emits -Wrestrict warnings which break the build because of -Werror
+# Needed for the AppImage:
+# - sandbox-allow-time: allow the time() syscall in the seccomp sandbox
+# - allow-readv-writev-in-seccomp-sandbox: curl/OpenSSL use readv() for TLS data (https://github.com/NixOS/nixpkgs/pull/539002)
+# - ca-certificates: allow reading CA bundles in the sandbox and support SSL_CERT_FILE
+for patch in ../patches/*.patch; do
+	patch -N -p1 --forward -i "$patch"
+done
+
+cmake \
+	--preset Distribution \
+	-B ./Build/distribution \
+	-S ./ \
+	-DENABLE_INSTALL_HEADERS=OFF \
+	-DCMAKE_INSTALL_PREFIX='/opt/ladybird/usr' \
+	-DCMAKE_INSTALL_LIBEXECDIR='lib/ladybird' \
+	-DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" \
+	-DVCPKG_ROOT="$VCPKG_ROOT" \
+	-Wno-dev
+
+cmake --build ./Build/distribution
+cmake --install ./Build/distribution
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,47 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q ladybird | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Base/res/icons/128x128/app-browser.png
 export DESKTOP=https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Meta/CMake/freedesktop/org.ladybird.Ladybird.desktop
-export ANYLINUX_LIB=1
 
 # Deploy dependencies
 quick-sharun \
-	/opt/ladybird/usr/bin/*          \
-	/opt/ladybird/usr/lib/*          \
-	/opt/ladybird/usr/lib/ladybird/* \
-	/opt/angle/usr/lib/*             \
-	/opt/ladybird/usr/share/*
-
-mv -v ./AppDir/lib/angle/usr/lib/* ./AppDir/lib
-
-# time() is blocked by the default sandbox rules
-# but clock_gettime() and gettimeofday() are allowed
-cat <<-'EOF' > ./AppDir/.timeshim.c
-#define _GNU_SOURCE
-#include <time.h>
-#include <sys/time.h>
-
-time_t time(time_t *t) {
-    struct timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) != 0) {
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-        ts.tv_sec = tv.tv_sec;
-    }
-    if (t)
-        *t = ts.tv_sec;
-    return ts.tv_sec;
-}
-EOF
-
-cc -shared -fPIC -O2 -o ./AppDir/lib/timeshim.so ./AppDir/.timeshim.c
-echo 'timeshim.so' >> ./AppDir/.preload
+	/opt/ladybird/usr/bin/* \
+	/opt/ladybird/usr/lib   \
+	/opt/ladybird/usr/share
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
